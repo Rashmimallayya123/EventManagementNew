974aaee015d9a417e17175fe64bfe40d003af85a
Rashmimallayya123 <rashmimathapati346@gmail.com>
2025-11-16 12:02:11 +0530

    Refactor admin login page with improved structure

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -53,9 +53,11 @@ session_start();
     <form action="admin_auth.php" method="POST">
         <input type="text" name="username" placeholder="Admin Username" required>
         <input type="password" name="password" placeholder="Password" required>
-        <button type="submit">Login</button>
+     <button type="submit" name="admin_login">Login</button>
+
     </form>
 </div>
 
 </body>
 </html>
+
