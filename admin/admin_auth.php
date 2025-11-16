2aa955855474fecd273e09037e12b4e5bf554ae0
Rashmimallayya123 <rashmimathapati346@gmail.com>
2025-11-16 12:01:30 +0530

    Refactor admin authentication process
    Updated the admin authentication logic to use a more secure method and improved session handling.

--- admin/admin_auth.php
+++ admin/admin_auth.php
@@ -2,19 +2,27 @@
 session_start();
 include("dbconnect.php");
 
+if (isset($_POST['admin_login'])) {
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-$query = "SELECT * FROM admin_users WHERE admin_username='$username' AND admin_password='$password'";
-$result = mysqli_query($conn, $query);
+    // Correct SQL according to your DB table
+    $sql = "SELECT * FROM admin_users WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($conn, $sql);
 
-if(mysqli_num_rows($result) == 1){
-    $_SESSION["admin_logged_in"] = true;
-    $_SESSION["admin_username"] = $username;
-    header("Location: admin_dashboard.php");
-} else {
-    header("Location: admin_login.php?error=1");
+    if (mysqli_num_rows($result) > 0) {
+
+        $admin = mysqli_fetch_assoc($result);
+
+        $_SESSION['admin_id'] = $admin['id'];
+        $_SESSION['admin_username'] = $admin['username'];
+
+        header("Location: admin_dashboard.php");
+        exit();
+
+    } else {
+        echo "<script>alert('Invalid admin login'); window.location.href='admin_login.php';</script>";
+    }
 }
 ?>
-
