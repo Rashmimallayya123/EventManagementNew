17b10497bf948ff9f5d85ca422155e34a2aaca96
Rashmimallayya123 <rashmimathapati346@gmail.com>
2025-11-16 19:03:09 +0530

    Update admin_auth.php

--- admin/admin_auth.php
+++ admin/admin_auth.php
@@ -2,28 +2,30 @@
 session_start();
 include("dbconnect.php");
 
-if (isset($_POST['admin_login'])) {
+if (!isset($_POST["admin_login"])) {
+    header("Location: admin_login.php");
+    exit();
+}
 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+$username = trim($_POST['username']);
+$password = trim($_POST['password']);
 
-    $sql = "SELECT * FROM admin_users WHERE username='$username' AND password='$password'";
-    $result = mysqli_query($conn, $sql);
+$sql = "SELECT * FROM admin_users WHERE username='$username' AND password='$password'";
+$result = mysqli_query($conn, $sql);
 
-    if (mysqli_num_rows($result) > 0) {
+if ($result && mysqli_num_rows($result) > 0) {
 
-        $admin = mysqli_fetch_assoc($result);
+    $admin = mysqli_fetch_assoc($result);
 
-        $_SESSION['admin_id'] = $admin['id'];
-        $_SESSION['admin_username'] = $admin['username'];
+    $_SESSION['admin_logged_in'] = true;
+    $_SESSION['admin_id'] = $admin['id'];
+    $_SESSION['admin_username'] = $admin['username'];
 
-        header("Location: admin_dashboard.php");
-        exit();
+    header("Location: admin_dashboard.php");
+    exit();
 
-    } else {
-        echo "<script>alert('Invalid admin login');
-              window.location.href='admin_login.php';
-              </script>";
-    }
+} else {
+    header("Location: admin_login.php?error=1");
+    exit();
 }
 ?>
